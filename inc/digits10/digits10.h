79d1c7ed465fa8e6b20359bec2eb0cd720417050
Eddie <nothing@not.com>
2017-08-12 18:29:41 -0700

    Tests running

--- inc/digits10/digits10.h
+++ inc/digits10/digits10.h
@@ -8,7 +8,7 @@ namespace pureconstexpr {
     }
 
     constexpr long unsigned exp10(unsigned exp) {
-        return exp < 1 ? 1 : 10 * exp10_constexpr(exp - 1);
+        return exp < 1 ? 1 : 10 * exp10(exp - 1);
     }
 
 }
@@ -30,46 +30,31 @@ namespace pureconstexpr {
 
 namespace detail {
 
-template<typename> struct Cutoff_helper;
+template<typename> struct Cutoff;
 template<unsigned long... p2s>
-struct Cutoff<IndexPack<unsigned long, p2
-    static long unsigned cutoff(unsigned a) {
+struct Cutoff<meta::IndexPack<unsigned long, p2s...>> {
+    static long unsigned cutoff(unsigned p2) {
         static constexpr unsigned long arr[] = {
             pureconstexpr::cutoff(1ul << p2s)...
         };
-        return arr[a];
+        return arr[p2];
+    }
+
+    static long unsigned ndigits(unsigned p2) {
+        static constexpr unsigned long arr[] = {
+            pureconstexpr::log10floor(1ul << p2s)...
+        };
+        return arr[p2];
     }
 };
-
-long cutoff(unsigned p2) {
-    using Ndxs = typename Indices<sizeof(long unsigned)*8 - 1>::type;
-    return Cutoff<Ndxs>::cutoff(p2);
-}
 
 }
 
 unsigned long digits10(unsigned long arg) {
     if(arg < 2) { return 1; }
     auto l2f = log2floor(arg);
-    auto cut = detail::cutoff(l2f);
-    auto adj = arg < cut ? 0 : 1;
-    return l2f/3 + adj;
-}
-
-#include <iostream>
-
-int main(int argc, const char *argv[]) {
-    for(int i = 0; i < 64; ++i) {
-        auto v = 1ul << i;
-        std::cout << i << ' ' << v << ' ' << cutoff(1ul) << std::endl;
-    }
-    std::cout <<
-        countDigits(9) << ' ' <<
-        countDigits(10) << ' ' <<
-        countDigits(99) << ' ' <<
-        countDigits(100) << ' ' <<
-        countDigits(65535) << ' ' <<
-        countDigits(65536) << std::endl;
-    std::cout << log2floor(8) << std::endl;
-    return 0;
+    using Combination = detail::Cutoff<meta::Indices<sizeof(long unsigned)*8 - 1>>; 
+    auto cut = arg < Combination::cutoff(l2f) ? 0 : 1;
+    auto base = 1 + Combination::ndigits(l2f);
+    return cut + base;
 }
--- test/test.cpp
+++ test/test.cpp
@@ -8,15 +8,31 @@
 
 #include "digits10/digits10.h"
 
+TEST_CASE("ZeroAndOne", "[digits10]") {
+    REQUIRE(1 == digits10(0));
+    REQUIRE(1 == digits10(1));
+}
+
 TEST_CASE("JustBefore3digits", "[digits10]") {
-	REQUIRE(2 == digits10(99));
+    REQUIRE(2 == digits10(99));
 }
 
 TEST_CASE("Starting3digits", "[digits10]") {
-	REQUIRE(3 == digits10(100));
+    REQUIRE(3 == digits10(100));
 }
 
 TEST_CASE("IndifirentToPowersOfTwo", "[digits10]") {
-	REQUIRE(5 == digits10((1 << 16) - 1));
-	REQUIRE(5 == digits10(1 << 16));
+    REQUIRE(5 == digits10((1 << 16) - 1));
+    REQUIRE(5 == digits10(1 << 16));
+}
+
+TEST_CASE("Comprehensive", "[digits10]") {
+    auto last = 1ul << 63;
+    auto expected = 1;
+    auto val = 1ul;
+    for(; val < last; val *= 10) {
+        REQUIRE(digits10(val) == expected);
+        ++expected;
+    }
+    REQUIRE(digits10(val) == expected);
 }
